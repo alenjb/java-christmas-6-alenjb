15885f26df7109f6fba6c9d0751ba309234bba61
alenjb <alenjb@naver.com>
2023-11-14 04:56:45 +0900

    ✨feat(domain):특별 할인 클래스 생성 및 유효성 검사 메서드들 구현
    특별 할인 클래스를 생성함.
    특별 할인 기간 유효성 검사 메서드와 주문 최소 금액 검사 메서드를 구현함.

---
+++ src/main/java/christmas/domain/SpecialEvent.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+import christmas.constants.Amount;
+import christmas.constants.Day;
+import christmas.constants.Menu;
+import christmas.constants.MenuCategory;
+
+public class SpecialEvent extends Discount implements Event {
+
+    @Override
+    public boolean isValidEventPeriod(int day) {
+        return Day.EVENT_START_DAY.contains(day);
+    }
+
+    @Override
+    public boolean isValidEventAmount(double totalAmount) {
+        return totalAmount >= Amount.MINIMUM_AMOUNT_FOR_EVENT.amount;
+    }
+}
