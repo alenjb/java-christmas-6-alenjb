05e528e02bd54b2f41ab87b4fff562ba488e7933
alenjb <alenjb@naver.com>
2023-11-14 03:22:35 +0900

    ✨feat(constants): 증정품 관련 열거형 클래스 구현
    증정품과 관련된 열거형 클래스를 구현함.

---
+++ src/main/java/christmas/constants/Giveaway.java
@@ -0,0 +1,12 @@
+package christmas.constants;
+
+public enum Giveaway {
+    CHAMPAGNE("샴페인"),
+    NONE("없음");
+
+    final String name;
+
+    Giveaway(String name) {
+        this.name = name;
+    }
+}
