b7ae5b6a8cd9bd7eaa81511d4f62c20bb823da82
alenjb <alenjb@naver.com>
2023-11-14 05:59:15 +0900

    ✨feat(domain):금액 계산 클래스에 총혜택 금액을 계산하는 메서드 구현
    금액 계산 클래스에 총혜택 금액을 계산하는 메서드를 구현함.

--- src/main/java/christmas/domain/CalculateAmount.java
+++ src/main/java/christmas/domain/CalculateAmount.java
@@ -10,11 +10,20 @@ public class CalculateAmount {
     GiveawayEvent giveawayEvent = new GiveawayEvent();
 
 
-    // 할인 전 총주문 금액을 계산한다.
-    public double calculateTotalAmountBeforeDiscount(Order order) {
+    // 할인 전 총주문 금액을 계산하는 메서드
+    public double getTotalAmountBeforeDiscount(Order order) {
         double resultAmount = 0;
         for (int amount : order.getOrderedMenus().values()) {
             resultAmount += amount;
+        }
+        return resultAmount;
+    }
+
+    // 총혜택 금액을 계산하는 메서드
+    public double getExpectedPaymentAmount(Order order) {
+        double resultAmount = 0;
+        for (Event event : order.getValidEvents()) {
+            resultAmount += event.calculateDiscountAmount(order);
         }
         return resultAmount;
     }
