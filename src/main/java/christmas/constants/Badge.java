ba292de2b624c5340b24203938f83c51b3beed56
alenjb <alenjb@naver.com>
2023-11-14 03:42:05 +0900

    ✨feat(constants): 뱃지 관련 열거형 클래스의 필드와 생성자 구현
    뱃지와 관련된 열거형 클래스의 필드와 생성자를 구현함.

---
+++ src/main/java/christmas/constants/Badge.java
@@ -0,0 +1,14 @@
+package christmas.constants;
+
+public enum Badge {
+    STAR("별"),
+    TREE("트리"),
+    SANTA("산타");
+
+    final String name;
+
+    Badge(String name) {
+        this.name = name;
+    }
+
+}
