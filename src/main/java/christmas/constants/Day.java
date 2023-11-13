4bc0423c1538830f6e852ccc2571cf18a994cceb
alenjb <alenjb@naver.com>
2023-11-13 17:25:01 +0900

    ✨feat(constants): 날짜 관련 열거형 클래스 구현
    날짜와 관련된 열거형 클래스를 구현함.

---
+++ src/main/java/christmas/constants/Day.java
@@ -0,0 +1,30 @@
+package christmas.constants;
+
+import java.util.List;
+
+public enum Day {
+
+    STAR_DAYS(List.of(3, 10, 17, 24, 25, 31)),
+    WEEKDAYS(List.of(3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31)), WEEKEND(List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)),
+    CHRISTMAS_D_DAY_EVENT_START_DAY(List.of(1)),
+    CHRISTMAS_D_DAY_EVENT_END_DAY(List.of(25)),
+    EVENT_START_DAY(List.of(1)),
+    EVENT_END_DAY(List.of(31));
+
+    private final List<Integer> days;
+
+    Day(List<Integer> days) {
+        this.days = days;
+    }
+
+    public List<Integer> getDays(){
+        return this.days;
+    }
+    public int getDay(){
+        return this.days.get(0);
+    }
+
+    public boolean contains(int day) {
+        return days.contains(day);
+    }
+}
