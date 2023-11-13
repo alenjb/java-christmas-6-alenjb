01d7460e1f7dae32a06a1ed31350445ace15f1e0
alenjb <alenjb@naver.com>
2023-11-14 06:17:18 +0900

    ✨feat(domain):금액 계산 클래스에 적용 이벤트들에 증정 이벤트가 있는지 확인하는 메서드 구현
    금액 계산 클래스에 적용 이벤트들에 증정 이벤트가 있는지 확인하는 메서드를 구현함.
    증정 이벤트가 있는 경우, 해당 메서드를 통해 (총혜택 금액 - 증정 이벤트 금액)을 할인 후 예상 결제 금액으로 계산하기 위해 해당 메서드를 구현함.

--- src/main/java/christmas/domain/CalculateAmount.java
+++ src/main/java/christmas/domain/CalculateAmount.java
@@ -1,6 +1,6 @@
 package christmas.domain;
 
-import christmas.constants.Menu;
+import java.util.List;
 
 public class CalculateAmount {
     ChristmasDdayEvent christmasDdayEvent = new ChristmasDdayEvent();
@@ -8,7 +8,6 @@ public class CalculateAmount {
     WeekendEvent weekendEvent = new WeekendEvent();
     SpecialEvent specialEvent = new SpecialEvent();
     GiveawayEvent giveawayEvent = new GiveawayEvent();
-
 
     // 할인 전 총주문 금액을 계산하는 메서드
     public double getTotalAmountBeforeDiscount(Order order) {
@@ -20,11 +19,21 @@ public class CalculateAmount {
     }
 
     // 총혜택 금액을 계산하는 메서드
-    public double getExpectedPaymentAmount(Order order) {
+    public double getTotalDiscountAmount(Order order) {
         double resultAmount = 0;
         for (Event event : order.getValidEvents()) {
             resultAmount += event.calculateDiscountAmount(order);
         }
         return resultAmount;
+    }
+
+    // 적용 이벤트들에 증정 이벤트가 있는지 확인하는 메서드
+    public boolean hasGiveawayEvent(List<Event> validEvents) {
+        for (Event event : validEvents) {
+            if (event instanceof GiveawayEvent) {
+                return true;
+            }
+        }
+        return false;
     }
 }
