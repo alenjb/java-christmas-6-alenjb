9d91b22930f8cfc29ca96806b605cda7419a8a62
alenjb <alenjb@naver.com>
2023-11-14 04:50:06 +0900

    ✨feat(domain):주말 할인 클래스의 메인 메뉴 개수 확인 메서드 구현
    주말 할인 클래스의 메인 메뉴 개수 확인 메서드를 구현함.

--- src/main/java/christmas/domain/WeekendEvent.java
+++ src/main/java/christmas/domain/WeekendEvent.java
@@ -9,11 +9,20 @@ public class WeekendEvent extends Discount implements Event {
 
     @Override
     public boolean isValidEventPeriod(int day) {
-        return Day.WEEKEND.contains(day);
+        return Day.WEEKDAYS.contains(day);
     }
 
     @Override
     public boolean isValidEventAmount(double totalAmount) {
         return totalAmount >= Amount.MINIMUM_AMOUNT_FOR_EVENT.amount;
+    }
+
+    public int getNumberOfMains(Order order) {
+        int numberOfMains = 0;
+        for (Menu menu : order.getOrderedMenus().keySet()) {
+            if (menu.getCategory().equals(MenuCategory.MAIN))
+                numberOfMains++;
+        }
+        return numberOfMains;
     }
 }
