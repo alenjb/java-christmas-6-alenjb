a11cf5b33ca7d2ef533da0b8d2c6580c893733b3
alenjb <alenjb@naver.com>
2023-11-14 04:39:53 +0900

    ✨feat(domain):평일 할인 클래스의 디저트 개수 확인 메서드 구현
    평일 할인 클래스의 디저트 개수 확인 메서드를 구현함.

--- src/main/java/christmas/domain/WeekdaysEvent.java
+++ src/main/java/christmas/domain/WeekdaysEvent.java
@@ -2,6 +2,8 @@ package christmas.domain;
 
 import christmas.constants.Amount;
 import christmas.constants.Day;
+import christmas.constants.Menu;
+import christmas.constants.MenuCategory;
 
 public class WeekdaysEvent extends Discount implements Event {
 
@@ -13,5 +15,14 @@ public class WeekdaysEvent extends Discount implements Event {
     @Override
     public boolean isValidEventAmount(double totalAmount) {
         return totalAmount >= Amount.MINIMUM_AMOUNT_FOR_EVENT.amount;
+    }
+
+    public int checkNumberOfDesserts(Order order) {
+        int numberOfDesserts = 0;
+        for (Menu menu : order.getOrderedMenus().keySet()) {
+            if (menu.getCategory().equals(MenuCategory.DESSERT))
+                numberOfDesserts++;
+        }
+        return numberOfDesserts;
     }
 }
