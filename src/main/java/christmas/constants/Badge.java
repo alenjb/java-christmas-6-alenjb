60475e8b26f6f1e70a9a1e9dafe582a577bcef4f
alenjb <alenjb@naver.com>
2023-11-14 03:47:31 +0900

    ✨feat(constants): 뱃지 관련 열거형 클래스의 해당 금액에 맞는 적절한 뱃지를 찾는 메서드 구현
    뱃지 관련 열거형에 NONE("없음")을 추가함.
    뱃지 관련 열거형 클래스의 해당 금액에 맞는 적절한 뱃지를 찾는 메서드 구현함.

--- src/main/java/christmas/constants/Badge.java
+++ src/main/java/christmas/constants/Badge.java
@@ -1,6 +1,9 @@
 package christmas.constants;
 
+import christmas.domain.Order;
+
 public enum Badge {
+    NONE("없음"),
     STAR("별"),
     TREE("트리"),
     SANTA("산타");
@@ -10,5 +13,17 @@ public enum Badge {
     Badge(String name) {
         this.name = name;
     }
+
+    // 해당 금액에 맞는 적절한 뱃지를 찾는 메서드
+    public Badge findAppropriateBadgeForAmount(Order order) {
+        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_SANTA_AMOUNT.amount)
+            return SANTA;
+        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_STAR_AMOUNT.amount)
+            return STAR;
+        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_TREE_AMOUNT.amount)
+            return TREE;
+        return NONE;
+    }
+
 
 }
