6afba7e2e964229632acb622af048f7bf47482d5
alenjb <alenjb@naver.com>
2023-11-14 03:50:06 +0900

    ✨feat(constants): 뱃지 관련 열거형 클래스의 뱃지를 부여하는 메서드 구현
    뱃지 관련 열거형 클래스의 뱃지를 부여하는 메서드를 구현함.

--- src/main/java/christmas/constants/Badge.java
+++ src/main/java/christmas/constants/Badge.java
@@ -3,10 +3,7 @@ package christmas.constants;
 import christmas.domain.Order;
 
 public enum Badge {
-    NONE("없음"),
-    STAR("별"),
-    TREE("트리"),
-    SANTA("산타");
+    NONE("없음"), STAR("별"), TREE("트리"), SANTA("산타");
 
     final String name;
 
@@ -16,13 +13,15 @@ public enum Badge {
 
     // 해당 금액에 맞는 적절한 뱃지를 찾는 메서드
     public Badge findAppropriateBadgeForAmount(Order order) {
-        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_SANTA_AMOUNT.amount)
-            return SANTA;
-        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_STAR_AMOUNT.amount)
-            return STAR;
-        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_TREE_AMOUNT.amount)
-            return TREE;
+        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_SANTA_AMOUNT.amount) return SANTA;
+        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_STAR_AMOUNT.amount) return STAR;
+        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_TREE_AMOUNT.amount) return TREE;
         return NONE;
+    }
+
+    // 뱃지를 부여하는 메서드
+    public void giveBadge(Order order) {
+        order.setBadge(findAppropriateBadgeForAmount(order));
     }
 
 
