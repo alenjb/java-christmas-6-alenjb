3ce30331414701107ed856929b50e202fbf6c5f6
alenjb <alenjb@naver.com>
2023-11-14 03:39:18 +0900

    ✨feat(constants): 금액 관련 열거형 클래스 구현
    금액과 관련된 열거형 클래스를 구현함.

---
+++ src/main/java/christmas/constants/Amount.java
@@ -0,0 +1,19 @@
+package christmas.constants;
+
+public enum Amount {
+    MINIMUM_AMOUNT_FOR_EVENT(10000), // 이벤트 최소 주문 금액 -10,000원
+    WEEKDAYS_DISCOUNT_AMOUNT(2023),// 평일 할인 금액 - 2023원
+    WEEKEND_DISCOUNT_AMOUNT(2023),// 주말 할인 금액 - 2023원
+    INITIAL_CHRISTMAS_DDAY_DISCOUNT_AMOUNT(1000),   // 크리스마스 할인 시작 금액
+    CHRISTMAS_DDAY_DISCOUNT_INCREMENT_AMOUNT_BY_DAY(100),  // 크리스마스 할인 증가분 금액
+    SPECIAL_DISCOUNT_AMOUNT(1000),  // 특별 할인 금액
+    MINIMUM_STAR_AMOUNT(5000),  // 스타뱃지 금액
+    MINIMUM_TREE_AMOUNT(10000),  // 트리뱃지 금액
+    MINIMUM_SANTA_AMOUNT(20000); // 산타 뱃지 금액
+
+    final double amount;
+
+    Amount(double amount) {
+        this.amount = amount;
+    }
+}
