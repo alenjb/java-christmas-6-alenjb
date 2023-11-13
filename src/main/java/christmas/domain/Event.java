315bd6f9c247c84d528470746cb61060f52fd067
alenjb <alenjb@naver.com>
2023-11-13 17:22:53 +0900

    fix(domian): EVENT 인터페이스 내부 메서드들의 파라미터 변경
    이벤트 기간인지 확인하는 메서드와 이벤트 가능 금액인지 확인하는 메서드에서 각각 날짜 정보와 총 주문 금액 정보가 필요하여 이를 파라미터에 추가함.

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -4,17 +4,17 @@ import java.util.List;
 
 public interface Event {
     // 유효한 이벤트인지 확인하는 메서드
-    boolean isValidEvent();
+    boolean isValidEvent(int day, double totalAmount);
 
     // 이벤트 기간인지 확인하는 메서드
-    boolean isValidEventPeriod();
+    boolean isValidEventPeriod(int day);
 
     // 이벤트 가능 금액인지 확인하는 메서드
-    boolean isValidEventAmount();
+    boolean isValidEventAmount(double totalAmount);
 
     // 유효한 이벤트 리스트에 해당 이벤트를 추가
     void addToValidEvents(List<Event> validEvents);
 
     // 이벤트를 실행하는 메서드
-    void doEvent(List<Event> validEvents);
+    void doEvent(Order order);
 }
