00dacd0b46ef240e1ab8cbee6dc70e423fdbf415
alenjb <alenjb@naver.com>
2023-11-15 05:30:47 +0900

    ✨feat(view):출력 클래스에 혜택 내역을 출력하는 기능 구현
    출력 클래스에 혜택 내역을 출력하는 기능을 구현함.

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,8 +3,7 @@ package christmas.view;
 import christmas.constants.Giveaway;
 import christmas.constants.Menu;
 import christmas.constants.Message;
-import christmas.domain.CalculateAmount;
-import christmas.domain.Order;
+import christmas.domain.*;
 
 public class OutputView {
     final CalculateAmount CALCULATOR = new CalculateAmount();
@@ -21,13 +20,13 @@ public class OutputView {
         }
     }
 
-    // 할인 전 총주문 금액을 출력한다.
+    // 할인 전 총주문 금액을 출력하는 메서드
     public void printTotalAmountBeforeDiscount(Order order) {
         System.out.println(Message.TOTAL_AMOUNT_BEFORE_DISCOUNT_MSG);
         System.out.println(String.format("%,d", CALCULATOR.getTotalAmountBeforeDiscount(order)) + "원");
     }
 
-    // 증정 메뉴를 출력한다.
+    // 증정 메뉴를 출력하는 메서드
     public void printGiveaway(Order order) {
         System.out.println(Message.GIVEAWAY_MSG);
         if (order.getGiveaway().equals(Giveaway.CHAMPAGNE)) {
@@ -35,5 +34,21 @@ public class OutputView {
             return;
         }
         System.out.println(Giveaway.NONE.name());
+    }
+
+    // 혜택 내역을 출력하는 메서드
+    public void printBenefits(Order order) {
+        for (Event event : order.getValidEvents()) {
+            if (event instanceof ChristmasDdayEvent)
+                System.out.println(Message.CHRISTMAS_DDAY_EVENT_MSG + String.format(" -%,d", event.calculateDiscountAmount(order)) + "원");
+            if (event instanceof WeekdaysEvent)
+                System.out.println(Message.WEEKDAYS_EVENT_MSG + String.format(" -%,d", event.calculateDiscountAmount(order)) + "원");
+            if (event instanceof WeekendEvent)
+                System.out.println(Message.WEEKEND_EVENT_MSG + String.format(" -%,d", event.calculateDiscountAmount(order)) + "원");
+            if (event instanceof SpecialEvent)
+                System.out.println(Message.SPECIAL_EVENT_MSG + String.format(" -%,d", event.calculateDiscountAmount(order)) + "원");
+            if (event instanceof GiveawayEvent)
+                System.out.println(Message.GIVEAWAY_MSG + String.format(" -%,d", ((GiveawayEvent) event).calculateGiveawayAmount(order)) + "원");
+        }
     }
 }
