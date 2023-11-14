17b911ff168d1b04566b4062abd85dfa87df5e37
alenjb <alenjb@naver.com>
2023-11-15 04:49:43 +0900

    ✨feat(util):주문 내용을 각 메뉴이름과 수량으로 분리하여 매핑하는 기능 구현
    입력을 파싱하는 기능 클래스에 주문 내용을 각 메뉴이름과 수량으로 분리하여 매핑하는 기능을 구현함.

--- src/main/java/christmas/util/Parser.java
+++ src/main/java/christmas/util/Parser.java
@@ -1,5 +1,7 @@
 package christmas.util;
 
+import christmas.constants.Menu;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -8,23 +10,38 @@ import java.util.Map;
 public class Parser {
 
     // 주문 내용을 각 메뉴이름과 수량으로 분리하여 매핑하는 메서드
-    public Map<String, Integer> splitInput(String inputs){
+    public Map<String, Integer> splitInput(String inputs) {
         Map<String, Integer> menus = new HashMap<>();
-        for(String menu: splitByComma(inputs)){
+        for (String menu : splitByComma(inputs)) {
             menus.putAll(splitByHyphen(menu));
         }
         return menus;
     }
 
+    // 주문 내용을 각 메뉴이름과 수량으로 분리하여 매핑하는 메서드
+    public Map<Menu, Integer> convertToMenuFormat(Map<String, Integer> inputs) {
+        Map<Menu, Integer> menus = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : inputs.entrySet()) {
+            Menu menu = Menu.getMenuByName(entry.getKey());
+            // 일치하는 메뉴 이름이 Menu 열거형에 있으면
+            if (menu != null) {
+                menus.put(menu, entry.getValue());
+            }
+        }
+
+        return menus;
+    }
+
     // 쉼표를 통해 주문 내용을 구분하는 메서드
-    public List<String> splitByComma(String inputs){
+    public List<String> splitByComma(String inputs) {
         // 쉼표로 문자열을 나누고 리스트로 변환
         String[] valuesArray = inputs.split(",");
         return Arrays.asList(valuesArray);
     }
 
     // 주문 내용을 주문메뉴와 수량을 구분하는 메서드
-    public Map<String, Integer> splitByHyphen(String inputs){
+    public Map<String, Integer> splitByHyphen(String inputs) {
         Map<String, Integer> resultMap = new HashMap<>();
         // "-"로 문자열을 나누기
         String[] parts = inputs.split("-");
