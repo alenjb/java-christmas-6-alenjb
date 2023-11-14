6aac7677e056ce6339565786260b64db361a778e
alenjb <alenjb@naver.com>
2023-11-14 21:11:54 +0900

    🎨fix(domain):주문 클래스의 생성자 수정 및 주문 메뉴에 메뉴를 추가하는 메서드 구현
    주문 클래스의 생성자에서 모든 메뉴의 수량을 0으로 초기화함.
    주문 메뉴를 set하는 메서드 이외에 주문 메뉴를 add하는 메서드를 추가함.

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -5,22 +5,47 @@ import christmas.constants.Giveaway;
 import christmas.constants.Menu;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Order {
+    final int NONE = 0;
     // 방문 날짜
     private int reservationDate;
     // 적용 가능한 이벤트들
     private List<Event> validEvents = new ArrayList<>();
     // 주문 메뉴들
-    private Map<Menu, Integer> orderedMenus;
+    private Map<Menu, Integer> orderedMenus = new HashMap<>();
     // 예상 결제 금액
     private int expectedPaymentAmount;
     // 배지를 NONE으로 초기화
     private Badge badge = Badge.NONE;
     //증정품을 NONE으로 초기화
     private Giveaway giveaway = Giveaway.NONE;
+
+    public Order() {
+        setOrderedMenus(
+                Map.ofEntries(
+                        // 애피타이저
+                        Map.entry(Menu.MUSHROOM_SOUP, NONE),
+                        Map.entry(Menu.TAPAS, NONE),
+                        Map.entry(Menu.CAESAR_SALAD, NONE),
+                        // 메인
+                        Map.entry(Menu.T_BONE_STEAK, NONE),
+                        Map.entry(Menu.BBQ_RIBS, NONE),
+                        Map.entry(Menu.SEAFOOD_PASTA, NONE),
+                        Map.entry(Menu.CHRISTMAS_PASTA, NONE),
+                        // 디저트
+                        Map.entry(Menu.CHOCOLATE_CAKE, NONE),
+                        Map.entry(Menu.ICE_CREAM, NONE),
+                        // 음료
+                        Map.entry(Menu.ZERO_COLA, NONE),
+                        Map.entry(Menu.RED_WINE, NONE),
+                        Map.entry(Menu.CHAMPAGNE, NONE)
+                )
+        );
+    }
 
     public int getReservationDate() {
         return reservationDate;
@@ -44,6 +69,14 @@ public class Order {
 
     public void setOrderedMenus(Map<Menu, Integer> orderedMenus) {
         this.orderedMenus = orderedMenus;
+    }
+    public void addOrderedMenus(Map<Menu, Integer> orderedMenus) {
+        // 맵을 복사하여 새로운 맵 생성
+        Map<Menu, Integer> existingMenus = new HashMap<>(getOrderedMenus());
+        // 새로운 맵에 새로운 매핑을 추가
+        existingMenus.putAll(orderedMenus);
+        // ORDER의 orderedMenus를 새로운 맵으로 갱신
+        setOrderedMenus(existingMenus);
     }
 
     public int getExpectedPaymentAmount() {
