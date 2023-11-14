8ebfdaa611dfba24b0166553c9cfb6f81eae111c
alenjb <alenjb@naver.com>
2023-11-15 05:28:10 +0900

    ✨feat(view):출력 클래스에 주문 메뉴를 출력하는 기능 구현
    출력 클래스에 주문 메뉴를 출력하는 기능을 구현함.

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,22 @@
 package christmas.view;
 
+import christmas.constants.Menu;
 import christmas.constants.Message;
+import christmas.domain.CalculateAmount;
+import christmas.domain.Order;
 
 public class OutputView {
+    final CalculateAmount CALCULATOR = new CalculateAmount();
+
     public void greet() {
         System.out.println(Message.GREETING_MSG);
+    }
+
+    // 주문 메뉴를 출력하는 메서드
+    public void printOrderedMenus(Order order) {
+        System.out.println(Message.MENU_MSG);
+        for (Menu menu : order.getOrderedMenus().keySet()) {
+            System.out.println(menu.getName() + " " + order.getOrderedMenus().get(menu) + "개");
+        }
     }
 }
