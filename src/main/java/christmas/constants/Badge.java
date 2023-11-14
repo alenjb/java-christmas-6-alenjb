817f65daf3cfb930fd2e0261bbc3dc54badb5207
alenjb <alenjb@naver.com>
2023-11-15 07:20:03 +0900

    🎨fix(constants):뱃지 열거형 금액 기준 변경 및 getter 생성
    뱃지 열거형 금액 기준을 예상 결제 금액에서 총 혜택 금액으로 변경함.
    뱃지 이름 getter를 생성함.

--- src/main/java/christmas/constants/Badge.java
+++ src/main/java/christmas/constants/Badge.java
@@ -1,11 +1,13 @@
 package christmas.constants;
 
+import christmas.domain.CalculateAmount;
 import christmas.domain.Order;
 
 public enum Badge {
     NONE("없음"), STAR("별"), TREE("트리"), SANTA("산타");
 
     final String name;
+    final CalculateAmount calculateAmount = new CalculateAmount();
 
     Badge(String name) {
         this.name = name;
@@ -13,9 +15,9 @@ public enum Badge {
 
     // 해당 금액에 맞는 적절한 뱃지를 찾는 메서드
     public Badge findAppropriateBadgeForAmount(Order order) {
-        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_SANTA_AMOUNT.amount) return SANTA;
-        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_STAR_AMOUNT.amount) return STAR;
-        if (order.getExpectedPaymentAmount() >= Amount.MINIMUM_TREE_AMOUNT.amount) return TREE;
+        if (calculateAmount.getTotalBenefits(order) >= Amount.MINIMUM_SANTA_AMOUNT.amount) return SANTA;
+        if (calculateAmount.getTotalBenefits(order) >= Amount.MINIMUM_STAR_AMOUNT.amount) return STAR;
+        if (calculateAmount.getTotalBenefits(order) >= Amount.MINIMUM_TREE_AMOUNT.amount) return TREE;
         return NONE;
     }
 
@@ -24,5 +26,7 @@ public enum Badge {
         order.setBadge(findAppropriateBadgeForAmount(order));
     }
 
-
+    public String getName() {
+        return name;
+    }
 }
