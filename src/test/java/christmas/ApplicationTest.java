6acc9878a4f916eeb95801769c95b6a4dadbca45
alenjb <alenjb@naver.com>
2023-11-15 14:00:41 +0900

    ✅test(christmas):주문 예외 테스트 구현
    아래 경우들의 날짜 예외 테스트들을 구현함.
      1. 메뉴 형식이 올바르지 않은 경우
         1. 마지막에 쉼표를 추가한 경우
         2. 마지막에 수량을 누락한 경우
         3. 마지막에 수량과 "-"를 누락한 경우
         4. 마지막에 수량을 누락하고 쉼표를 누락한 경우
         5. 1개의 메뉴를 주문하면서 쉼표를 추가한 경우
         6. 1개의 메뉴를 주문하면서 수량과 "-"를 누락한 경우
         7. 빈 값인 경우
      2. 중복 메뉴를 입력한 경우
      3. 메뉴판에 없는 메뉴를 입력한 경우
      4. 음료를 제외한 음식을 1개 이상 주문하지 않은 경우
      5. 메뉴를 한번에 20개를 초과해서 주문한 경우
      6. 메뉴의 개수에 1 미만의 값을 입력한 경우

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -13,15 +13,7 @@ class ApplicationTest extends NsTest {
     void 모든_타이틀_출력() {
         assertSimpleTest(() -> {
             run("3", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
-            assertThat(output()).contains(
-                    "<주문 메뉴>",
-                    "<할인 전 총주문 금액>",
-                    "<증정 메뉴>",
-                    "<혜택 내역>",
-                    "<총혜택 금액>",
-                    "<할인 후 예상 결제 금액>",
-                    "<12월 이벤트 배지>"
-            );
+            assertThat(output()).contains("<주문 메뉴>", "<할인 전 총주문 금액>", "<증정 메뉴>", "<혜택 내역>", "<총혜택 금액>", "<할인 후 예상 결제 금액>", "<12월 이벤트 배지>");
         });
     }
 
@@ -61,8 +53,65 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 주문_예외_테스트() {
+        // 1. 메뉴 형식이 올바르지 않은 경우
+        // 1-a. 마지막에 쉼표를 추가한 경우
         assertSimpleTest(() -> {
-            runException("3", "제로콜라-a");
+            runException("3", "해산물파스타-2,레드와인-1,초코케이크-1,");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 1-b. 마지막에 수량을 누락한 경우
+        assertSimpleTest(() -> {
+            runException("3", "해산물파스타-2,레드와인-1,초코케이크-1,레드-");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 1-c. 마지막에 수량과 "-"를 누락한 경우
+        assertSimpleTest(() -> {
+            runException("3", "해산물파스타-2,레드와인-1,초코케이크-1,레드");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 1-d. 마지막에 수량을 누락하고 쉼표를 누락한 경우
+        assertSimpleTest(() -> {
+            runException("3", "해산물파스타-2,레드와인-1,초코케이크-1레드-");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 1-e. 1개의 메뉴를 주문하면서 쉼표를 추가한 경우
+        assertSimpleTest(() -> {
+            runException("3", "해산물파스타-2,");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 1-f. 1개의 메뉴를 주문하면서 수량과 "-"를 누락한 경우
+        assertSimpleTest(() -> {
+            runException("3", "해산물파스타");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 1-g. 빈 값인 경우
+        assertSimpleTest(() -> {
+            runException("3", " ");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 2, 중복 메뉴를 입력한 경우
+        assertSimpleTest(() -> {
+            runException("3", "해산물파스타-2,레드와인-1,해산물파스타-1");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 3. 메뉴판에 없는 메뉴를 입력한 경우
+        assertSimpleTest(() -> {
+            runException("3", "해산물파스타-2,당근케이크-3");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 4. 음료를 제외한 음식을 1개 이상 주문하지 않은 경우
+        assertSimpleTest(() -> {
+            runException("3", "레드와인-5,제로콜라-4");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 5. 메뉴를 한번에 20개를 초과해서 주문한 경우
+        assertSimpleTest(() -> {
+            runException("3", "해산물파스타-5,제로콜라-12,초코케이크-8");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+        // 6. 메뉴의 개수에 1 미만의 값을 입력한 경우
+        assertSimpleTest(() -> {
+            runException("3", "제로콜라-0,해산물파스타-3");
             assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         });
     }
