85f0f11b1df2b4e6e4e52a7e4bac201d52cf481e
alenjb <alenjb@naver.com>
2023-11-15 15:14:36 +0900

    ✅test(christmas):통합 테스트 구현
    아래 경우들의 통합 테스트들을 구현함.
    1. 증정 이벤트가 적용되는 경우
    2. 증정 이벤트가 적용되는 경우

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -3,6 +3,9 @@ package christmas;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,7 +24,7 @@ class ApplicationTest extends NsTest {
     void 혜택_내역_없음_출력() {
         assertSimpleTest(() -> {
             run("26", "타파스-1,제로콜라-1");
-            assertThat(output()).contains("<혜택 내역>" + LINE_SEPARATOR + "없음");
+            assertThat(output()).contains("<혜택 내역>", LINE_SEPARATOR, "없음");
         });
     }
 
@@ -114,6 +117,73 @@ class ApplicationTest extends NsTest {
             runException("3", "제로콜라-0,해산물파스타-3");
             assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         });
+    }
+
+    @Test
+    void 증정_이벤트_적용_정상_기능_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("25", "해산물파스타-2,티본스테이크-2,레드와인-2,초코케이크-1");
+                    assertThat(output()).contains(
+                            "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
+                            "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)",
+                            "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)",
+                            "12월 25일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",
+                            "<주문 메뉴>",
+                            "레드와인 2개",
+                            "해산물파스타 2개",
+                            "초코케이크 1개",
+                            "티본스테이크 2개",
+                            "<할인 전 총주문 금액>",
+                            "315,000원",
+                            "<증정 메뉴>",
+                            "샴페인 1개",
+                            "<혜택 내역>",
+                            "크리스마스 디데이 할인: -3,400원",
+                            "평일 할인 -2,023원",
+                            "특별 할인 -1,000원",
+                            "증정 이벤트 -25,000원",
+                            "<총혜택 금액>",
+                            "-31,423원",
+                            "<할인 후 예상 결제 금액>",
+                            "308,577원",
+                            "<12월 이벤트 배지>",
+                            "산타"
+                    );
+                },
+                List.of());
+    }
+
+    @Test
+    void 증정_이벤트_미적용_정상_기능_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("25", "티본스테이크-1,초코케이크-1");
+                    assertThat(output()).contains(
+                            "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
+                            "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)",
+                            "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)",
+                            "12월 25일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",
+                            "<주문 메뉴>",
+                            "초코케이크 1개",
+                            "티본스테이크 1개",
+                            "<할인 전 총주문 금액>",
+                            "70,000원",
+                            "<증정 메뉴>",
+                            "없음",
+                            "<혜택 내역>",
+                            "크리스마스 디데이 할인: -3,400원",
+                            "평일 할인 -2,023원",
+                            "특별 할인 -1,000원",
+                            "<총혜택 금액>",
+                            "-6,423원",
+                            "<할인 후 예상 결제 금액>",
+                            "63,577원",
+                            "<12월 이벤트 배지>",
+                            "별"
+                    );
+                },
+                List.of());
     }
 
     @Override
